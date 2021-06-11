b13c02080e3f44c27c0e70a055c68162549a4e0b
Jessenia Toapanta <jessytpt.dev@gmail.com>
2021-06-10 20:52:08 -0500

    Http Post

--- Todolist_App/Controllers/TodolistController.cs
+++ Todolist_App/Controllers/TodolistController.cs
@@ -16,10 +16,30 @@ namespace Todolist_App.Controllers
             _context = context;
         }
 
+        //Http Get Index
         public IActionResult Index()
         {
             IEnumerable<Tarea> listaTareas = _context.Tareas;
             return View(listaTareas);
+        }
+
+        //Http Get Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Http Post Create
+        [HttpPost]
+        public IActionResult Create(Tarea tarea)
+        {
+            if (ModelState.IsValid) {
+                tarea.EstadoTarea = false;
+                _context.Tareas.Add(tarea);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }

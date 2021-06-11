58c9d7a420538a9f6bba47763e885bbae29d5409
Jessenia Toapanta <jessytpt.dev@gmail.com>
2021-06-10 23:28:03 -0500

    CRUD completo

--- Todolist_App/Controllers/TodolistController.cs
+++ Todolist_App/Controllers/TodolistController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,9 +38,128 @@ namespace Todolist_App.Controllers
                 tarea.EstadoTarea = false;
                 _context.Tareas.Add(tarea);
                 _context.SaveChanges();
+
+                TempData["mensaje"] = "Tarea creada correctamente!";
                 return RedirectToAction("Index");
             }
             return View();
         }
+
+        //Http Get Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) {
+                return NotFound();
+
+            }
+
+            var tarea = _context.Tareas.Find(id);
+
+            if (tarea == null)
+            {
+                return NotFound();
+            }
+
+            return View(tarea);
+        }
+
+        //Http Post Edit
+        [HttpPost]
+        public IActionResult Edit(Tarea tarea)
+        {
+            if (ModelState.IsValid)
+            {
+                tarea.EstadoTarea = false;
+                _context.Tareas.Update(tarea);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "Tarea actualizada correctamente!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //Http Get Delete
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+
+            //-------------
+            if (id == null || id == 0)
+            {
+                return NotFound();
+
+            }
+
+            var tarea = _context.Tareas.Find(id);
+
+            if (tarea == null)
+            {
+                return NotFound();
+            }
+
+            return View(tarea);
+        }
+
+        //Http Post Delete
+        [HttpPost]
+        public IActionResult Eliminar(int? id)
+        {
+            var tarea = _context.Tareas.Find(id);
+
+            if (tarea == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tareas.Remove(tarea);
+            _context.SaveChanges();
+
+            TempData["mensaje"] = "Tarea eliminada!";
+            return RedirectToAction("Index");
+        }
+
+        //Http Post Delete
+        [HttpPost]
+        //  [ActionName("Delete")]
+        public ActionResult DeleteTarea(IFormCollection formCollection)
+        {
+            int _idTarea = Convert.ToInt32(formCollection["IdTarea"]);
+
+            var tarea = _context.Tareas.Find(_idTarea);
+
+            if (tarea == null)
+            {
+                return NotFound();
+            }
+
+             _context.Tareas.Remove(tarea);
+             _context.SaveChanges();
+
+             TempData["mensaje"] = "Tarea eliminada!";
+             return RedirectToAction("Index");
+        
+
+        }        
+        /*
+        //Http Post Delete
+        [HttpPost]
+      //  [ActionName("Delete")]
+        public ActionResult DeleteTarea(int id)
+        {
+            var tarea = _context.Tareas.Find(id);
+
+            if (tarea == null ) {
+                return NotFound();
+            }
+           
+                _context.Tareas.Remove(tarea);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "Tarea eliminada!";
+                return RedirectToAction("Index");
+            
+        }*/
+
     }
 }
